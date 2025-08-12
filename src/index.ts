4bda6d6ea8bbe114ea5d1aa878ef979251212193
vladislav.aleynikov <vladislav.aleinikov@innowise.com>
2025-08-12 20:23:05 +0300

    add main ts file

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { Button } from "./components/button";
+import { Checkbox } from "./components/checkbox";
+import { Modal } from "./components//modal";
+import { Select } from "./components/select";
+import { Switch } from "./components/switch";
+import { TextField } from "./components/text-field";
+
+export { Button, Checkbox, Modal, Select, Switch, TextField };
